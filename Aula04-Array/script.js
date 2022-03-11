a663eed9bbaa34a113899a645dd2aa28f149c19a
Rodrigo Queiroz <91379269+RodrigoBagage@users.noreply.github.com>
2022-03-11 17:19:29 -0300

    Aula04

---
+++ Aula04-Array/script.js
@@ -0,0 +1,14 @@
+var listaFilmes = [
+    "https://br.web.img3.acsta.net/pictures/20/06/23/20/57/4275033.jpg",
+    "https://br.web.img3.acsta.net/pictures/22/03/02/19/26/3666027.jpg",
+    "https://br.web.img2.acsta.net/pictures/20/08/10/22/00/5069470.jpg",
+    "https://br.web.img2.acsta.net/pictures/17/09/29/21/15/4233147.jpg",
+    "https://br.web.img3.acsta.net/pictures/21/06/22/22/19/1495362.jpg",
+    "https://img.elo7.com.br/product/zoom/2A1A4BF/big-poster-filme-joker-coringa-joaquin-phoenix-lo02-90x60-cm-poster.jpg",
+    
+  ];
+  
+  for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">");
+  }
+  
